5254bca6c9d1f95630042ef1c015c4ec7e1e2f21
Briankimp <bkimathi973@gmail.com>
2025-01-27 12:15:47 +0300

    Analytics Done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,21 @@
-import type { Metadata } from 'next'
-import './globals.css'
+import type { Metadata } from "next";
+import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
-  title: 'Geoffry Accounting',
-  description: 'Website for an accounting firm. We deliver exceprional results',
-}
+  title: "Geoffry Accounting",
+  description: "Website for an accounting firm. We deliver exceprional results",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
+      <Analytics />
       <body>{children}</body>
     </html>
-  )
+  );
 }
