118cad362154c61c8a488b4b6bc21663684e3628
Briankimp <bkimathi973@gmail.com>
2025-01-27 08:56:46 +0300

    Changes Metadat

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Geoffry Accounting',
+  description: 'Website for an accounting firm. We deliver exceprional results',
 }
 
 export default function RootLayout({
